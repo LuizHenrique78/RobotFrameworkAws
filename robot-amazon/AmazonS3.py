576d497045276472c688c06a45a658fc69345cb6
Luiz Henrique Rodrigues <luiz.rlemos345@gmail.com>
2023-07-24 12:19:33 -0300

    Add tests and keywords

--- robot-amazon/AmazonS3.py
+++ robot-amazon/AmazonS3.py
@@ -3,6 +3,8 @@ import botocore
 from json import loads
 from datetime import datetime
 
+import os
+
 class AmazonS3():
     def __init__(self, aws_region: str, aws_acces_key: str, aws_secret_key: str) -> None:
 
@@ -59,17 +61,60 @@ class AmazonS3():
     def delete_buckets(self, bucket_name):
         try:
             response = self.client.list_objects_v2(Bucket=bucket_name)
-
             if 'Contents' in response:
                 objects = [{'Key': obj['Key']} for obj in response['Contents']]
                 self.client.delete_objects(Bucket=bucket_name, Delete={'Objects': objects})    
                 print(f"Todos os objetos foram excluídos do bucket '{bucket_name}'.")   
 
-            self.client.delete_bucket(Bucket=bucket_name)
+            response = self.client.delete_bucket(Bucket=bucket_name)
             print(f"Bucket '{bucket_name}' excluído permanentemente.")
+            return  response
 
         except Exception as e:
             print(f"Erro ao excluir o bucket '{bucket_name}': {e}")
+
+    def upload_folder_content(self, folder_path, bucket, object_name=None):
+        if object_name is None:
+            object_name = os.path.basename(folder_path)
+            print(object_name)
+
+        for files in os.listdir(f'{folder_path}'):
+            try:
+                self.client.upload_file(f'{folder_path}/{files}',bucket,f"{object_name}/{files}")
+                print(f"FILE FROM {folder_path} UPLOADED SUCCESFULLY file: {files}")
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == "NoSuchBucket":
+                    print("O bucket não existe")
+                    return False, e.response['Error']['Code']
+                elif e.response['Error']['Code'] == "AccessDenied":
+                    print("O acesso ao bucket foi negado")
+                    return False, e.response['Error']['Code']
+                else:
+                    print("Ocorreu um erro inesperado:", e)
+                    return False, e.response['Error']['Code']
+        return True, None    
+    
+    def upload_file(self, file_name, bucket, object_name=None):
+        """Upload a file to an S3 bucket
+
+        :param file_name: File to upload
+        :param bucket: Bucket to upload to
+        :param object_name: S3 object name. If not specified then file_name is used
+        :return: True if file was uploaded, else False
+        """
+
+        # If S3 object_name was not specified, use file_name
+        if object_name is None:
+            object_name = os.path.basename(file_name)
+            print(object_name)
+
+        # Upload the file
+    
+        try:
+            response = self.client.upload_file(file_name, bucket, object_name)
+        except botocore.exceptions.ClientError as error:
+            raise error
+        return True
 
     def upload_folder_content(self):
         pass
--- robot-amazon/tests/resources/base.robot
+++ robot-amazon/tests/resources/base.robot
@@ -2,4 +2,13 @@
 
 Library         ../../AmazonS3.py        %{REGION}            %{ACESSKEY}            %{SECRETKEY}
 
+
+Library    String
+Library    Collections
+Library    random
+Library    BuiltIn
+
+
 Resource         actions/utils.robot
+Resource         actions/variables.robot
+Resource         actions/Assertions/Assertions.robot
